ac0a3af8271df69e14deafc445dc535780677c91
PC201609042206\Administrator <spogxml@gmail.com>
2016-12-10 22:42:30 +0800

    修改了删除和更新数据功能

--- PasswordManager/src/com/example/passwordmanager/dboperate/MyDataDB.java
+++ PasswordManager/src/com/example/passwordmanager/dboperate/MyDataDB.java
@@ -17,65 +17,59 @@ import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
 public class MyDataDB {
+	private static String DB_NAME="pwmg.db";
+	
 	//创建数据库，如果存在则打开
-	public static SQLiteDatabase getSQLiteDatabase(Context context,String dbname){
-		MDHelper helper = new MDHelper(context, dbname, null, 1);
+	public static SQLiteDatabase getSQLiteDatabase(Context context){
+		MDHelper helper = new MDHelper(context, DB_NAME, null, 1);
 		return helper.getWritableDatabase();
 	}
 	//插入数据
-	public int insertDB(Context context,MyData data,String key){
-		long res=1;
-		long res1=1;
-		long res2=1;
-		long res3=1;
-		if(key!=null||!("".equals(key))){
-			defkey=key;
-		}else{
-			defkey="123456";
-		}
-		ContentValues cv = new ContentValues();
-		if(data.title!=null){
-			cv.put("title", data.title);
-			cv.put("user", data.user);
-			//将密码进行编码后存储，默认使用defkey,如果设置过加密密码，则修改defkey的值。
-			String s=data.password;
-			cv.put("password",EncryptUitl.encode(s,defkey));
-			Log.i("info", "插入数据时defkey为："+defkey);
-			cv.put("note", data.note);
-			res1=pwdb.insert("pwtb", null, cv);
-			cv.clear();
-		}//设置登陆密码
-		else if(data.lgpassword!=null){
-			//将登陆密码进行编码后存储
-			String s1=data.lgpassword;
-			cv.put("lgpassword", EncryptUitl.encode(s1,"123456"));
-			res2=pwdb.insert("ustb", "lgpassword", cv);
-			cv.clear();
-		}//设置加密密码
-		else if(data.itpassword!=null){
-			//将加密密码进行编码后存储
-			String s2=data.itpassword;
-			cv.put("itpassword", EncryptUitl.encode(s2,"123456"));
-			res3=pwdb.insert("ustb", "itpassword", cv);
-			cv.clear();
-		}else{
-			res=-1;
-		}
-		pwdb.close();
-		if (res1 == -1||res2==-1||res3==-1||res==-1) {  
-			//添加失败  
-			return 0;
-		} else {  
-			//添加成功 
-			return 1;
-		}
+	public static void insertDB(SQLiteDatabase pwdb,String st,String[] values){
+		pwdb.execSQL(st,values);
+//		ContentValues cv = new ContentValues();
+//		if(data.title!=null){
+//			cv.put("title", data.title);
+//			cv.put("user", data.user);
+//			//将密码进行编码后存储，默认使用defkey,如果设置过加密密码，则修改defkey的值。
+//			String s=data.password;
+//			cv.put("password",EncryptUitl.encode(s,defkey));
+//			Log.i("info", "插入数据时defkey为："+defkey);
+//			cv.put("note", data.note);
+//			res1=pwdb.insert("pwtb", null, cv);
+//			cv.clear();
+//		}//设置登陆密码
+//		else if(data.lgpassword!=null){
+//			//将登陆密码进行编码后存储
+//			String s1=data.lgpassword;
+//			cv.put("lgpassword", EncryptUitl.encode(s1,"123456"));
+//			res2=pwdb.insert("ustb", "lgpassword", cv);
+//			cv.clear();
+//		}//设置加密密码
+//		else if(data.itpassword!=null){
+//			//将加密密码进行编码后存储
+//			String s2=data.itpassword;
+//			cv.put("itpassword", EncryptUitl.encode(s2,"123456"));
+//			res3=pwdb.insert("ustb", "itpassword", cv);
+//			cv.clear();
+//		}else{
+//			res=-1;
+//		}
+//		pwdb.close();
+//		if (res1 == -1||res2==-1||res3==-1||res==-1) {  
+//			//添加失败  
+//			return 0;
+//		} else {  
+//			//添加成功 
+//			return 1;
+//		}
 	}
 
 	//查询数据
-	public static List<Map<String,Object>> queryDB(SQLiteDatabase pwdb ,String st){
+	public static List<Map<String,Object>> queryDB(SQLiteDatabase pwdb ,String st,String[] values){
 		List<Map<String,Object>> arr_list = new ArrayList<Map<String,Object>>();
 		Cursor c;
-		c=pwdb.rawQuery(st,null);
+		c=pwdb.rawQuery(st,values);
 		if(c!=null&&c.getCount()!=0){
 			while(c.moveToNext()){
 				Map<String,Object> HM = new HashMap<String,Object>();
@@ -110,153 +104,157 @@ public class MyDataDB {
 		return arr_list;
 	}
 
-	//修改数据
-	public int updateDB(MyData udata,String st,String key){
-		long res1;
-		long res2;
-		long res3;
-		if(key!=null||!("".equals(key))){
-			defkey=key;
-		}else{
-			defkey="123456";
-		}
-		ContentValues cv = new ContentValues();
-		if(udata.title!=null){
-			cv.put("title", udata.title);
-			cv.put("user", udata.user);
-			//将修改后的密码编码后更新
-			String s=udata.password;
-			cv.put("password", EncryptUitl.encode(s,defkey));
-			cv.put("note", udata.note);
-			//更新数据
-			res1=pwdb.update("pwtb", cv, "title=?", new String[]{st});
-			cv.clear();
-		}else
-			res1=1;
-		//修改登陆密码
-		if(udata.lgpassword!=null){
-			//将登陆密码进行编码后更新
-			String s1=udata.lgpassword;
-			cv.put("lgpassword", EncryptUitl.encode(s1,"123456"));
-			//更新密码
-			res2=pwdb.update("ustb", cv, "lgpassword=?", new String[]{EncryptUitl.encode(st,"123456")});
-			cv.clear();
-		}else 
-			res2=1;
-		//修改加密密码
-		if(udata.itpassword!=null){
-			//将加密密码进行编码后更新
-			String s2=udata.itpassword;
-			cv.put("itpassword", EncryptUitl.encode(s2,"123456"));
-			//更新密码
-			res3=pwdb.update("ustb", cv, "itpassword=?", new String[]{EncryptUitl.encode(st,"123456")});
-			cv.clear();
-		}else
-			res3=1;
-		pwdb.close();
-		if (res1 == -1||res2==-1||res3==-1) {  
-			//更新失败  
-			return 0;
-		} else {  
-			//更新成功 
-			return 1;
-		}
+	//更新数据
+	public static void updateDB(SQLiteDatabase pwdb,String st,String[] values){
+		pwdb.execSQL(st,values);
+//		long res1;
+//		long res2;
+//		long res3;
+//		if(key!=null||!("".equals(key))){
+//			defkey=key;
+//		}else{
+//			defkey="123456";
+//		}
+//		ContentValues cv = new ContentValues();
+//		if(udata.title!=null){
+//			cv.put("title", udata.title);
+//			cv.put("user", udata.user);
+//			//将修改后的密码编码后更新
+//			String s=udata.password;
+//			cv.put("password", EncryptUitl.encode(s,defkey));
+//			cv.put("note", udata.note);
+//			//更新数据
+//			res1=pwdb.update("pwtb", cv, "title=?", new String[]{st});
+//			cv.clear();
+//		}else
+//			res1=1;
+//		//修改登陆密码
+//		if(udata.lgpassword!=null){
+//			//将登陆密码进行编码后更新
+//			String s1=udata.lgpassword;
+//			cv.put("lgpassword", EncryptUitl.encode(s1,"123456"));
+//			//更新密码
+//			res2=pwdb.update("ustb", cv, "lgpassword=?", new String[]{EncryptUitl.encode(st,"123456")});
+//			cv.clear();
+//		}else 
+//			res2=1;
+//		//修改加密密码
+//		if(udata.itpassword!=null){
+//			//将加密密码进行编码后更新
+//			String s2=udata.itpassword;
+//			cv.put("itpassword", EncryptUitl.encode(s2,"123456"));
+//			//更新密码
+//			res3=pwdb.update("ustb", cv, "itpassword=?", new String[]{EncryptUitl.encode(st,"123456")});
+//			cv.clear();
+//		}else
+//			res3=1;
+//		pwdb.close();
+//		if (res1 == -1||res2==-1||res3==-1) {  
+//			//更新失败  
+//			return 0;
+//		} else {  
+//			//更新成功 
+//			return 1;
+//		}
 
 	}
 
 	//删除数据
-	public int deleteDB(String st){
-		long res=0;
-		if(st.equals("deleteall")){
-			res=pwdb.delete("pwtb", null, null);
-		}else{
-			res=pwdb.delete("pwtb", "title=?", new String[]{st});
-		}
-		if(res==0){
-			//删除失败
-			return 0;
-		}else{
-			//删除成功
-			return 1;
-		}
+	public static void deleteDB(SQLiteDatabase pwdb,String st,String[] values){
+		pwdb.execSQL(st,values);
+//		long res=0;
+//		if(st.equals("deleteall")){
+//			res=pwdb.delete("pwtb", null, null);
+//		}else{
+//			res=pwdb.delete("pwtb", "title=?", new String[]{st});
+//		}
+//		if(res==0){
+//			//删除失败
+//			return 0;
+//		}else{
+//			//删除成功
+//			return 1;
+//		}
+//	}
+//	//删除密码
+//	public void deletePW(){
+//		pwdb.delete("ustb", null, null);
+//		pwdb.close();
+//	}
+//	//查询登陆密码
+//	public int queryLgPW(String st){
+//		int res=0;
+//		//如果为空
+//		if("".equals(st)||st==null){
+//			//查询所有数据
+//			Cursor c;
+//			c=pwdb.query("ustb",null, "lgpassword>?", new String[]{"0"}, null, null, null);
+//			if(c!=null&&c.getCount()!=0){
+//				//存在数据则返回1
+//				res=1;
+//				c.close();
+//			}else{
+//				//不存在数据则返回0
+//				res=0;
+//				c.close();
+//			}
+//		}//如果不为空
+//		else{
+//			//将密码进行编码后匹配数据库查询
+//			//查询
+//			Cursor c;
+//			c=pwdb.query("ustb", null, "lgpassword Like ?", new String[]{EncryptUitl.encode(st,"123456")}, null, null, null);
+//			if(c!=null&&c.getCount()!=0){
+//				//查询成功
+//				c.close();
+//				res=1;
+//			}
+//			else{
+//				//查询失败
+//				c.close();
+//				res=0;
+//			}
+//		}
+//
+//		return res;
 	}
-	//删除密码
-	public void deletePW(){
-		pwdb.delete("ustb", null, null);
-		pwdb.close();
-	}
-	//查询登陆密码
-	public int queryLgPW(String st){
-		int res=0;
-		//如果为空
-		if("".equals(st)||st==null){
-			//查询所有数据
-			Cursor c;
-			c=pwdb.query("ustb",null, "lgpassword>?", new String[]{"0"}, null, null, null);
-			if(c!=null&&c.getCount()!=0){
-				//存在数据则返回1
-				res=1;
-				c.close();
-			}else{
-				//不存在数据则返回0
-				res=0;
-				c.close();
-			}
-		}//如果不为空
-		else{
-			//将密码进行编码后匹配数据库查询
-			//查询
-			Cursor c;
-			c=pwdb.query("ustb", null, "lgpassword Like ?", new String[]{EncryptUitl.encode(st,"123456")}, null, null, null);
-			if(c!=null&&c.getCount()!=0){
-				//查询成功
-				c.close();
-				res=1;
-			}
-			else{
-				//查询失败
-				c.close();
-				res=0;
-			}
-		}
-
-		return res;
-	}
-	//查询加密密码
-	public int queryItPW(String st){
-		int res=0;
-		//如果为空
-		if("".equals(st)||st==null){
-			//查询所有数据
-			Cursor c;
-			c=pwdb.query("ustb", null, "itpassword>?", new String[]{"0"}, null, null, null);
-			if(c!=null&&c.getCount()!=0){
-				//存在数据则返回1
-				c.close();
-				res=1;
-			}else{
-				//不存在数据则返回0
-				c.close();
-				res=0;
-			}
-		}//如果不为空
-		else{
-			//将密码进行编码后匹配数据库查询
-			//查询
-			Cursor c;
-			c=pwdb.query("ustb", null, "itpassword Like ?", new String[]{EncryptUitl.encode(st,"123456")}, null, null, null);
-			if(c!=null&&c.getCount()!=0){
-				//查询成功
-				c.close();
-				res=1;
-			}
-			else{
-				//查询失败
-				c.close();
-				res=0;
-			}
-		}
-		return res;
-	}
+	
+	
+//	//查询加密密码
+//	public int queryItPW(String st){
+//		int res=0;
+//		//如果为空
+//		if("".equals(st)||st==null){
+//			//查询所有数据
+//			Cursor c;
+//			c=pwdb.query("ustb", null, "itpassword>?", new String[]{"0"}, null, null, null);
+//			if(c!=null&&c.getCount()!=0){
+//				//存在数据则返回1
+//				c.close();
+//				res=1;
+//			}else{
+//				//不存在数据则返回0
+//				c.close();
+//				res=0;
+//			}
+//		}//如果不为空
+//		else{
+//			//将密码进行编码后匹配数据库查询
+//			//查询
+//			Cursor c;
+//			c=pwdb.query("ustb", null, "itpassword Like ?", new String[]{EncryptUitl.encode(st,"123456")}, null, null, null);
+//			if(c!=null&&c.getCount()!=0){
+//				//查询成功
+//				c.close();
+//				res=1;
+//			}
+//			else{
+//				//查询失败
+//				c.close();
+//				res=0;
+//			}
+//		}
+//		return res;
+//	}
 
 }
--- PasswordManager/src/com/example/passwordmanager/dboperate/MydataOperate.java
+++ PasswordManager/src/com/example/passwordmanager/dboperate/MydataOperate.java
@@ -1,30 +1,214 @@
 package com.example.passwordmanager.dboperate;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import com.example.passwordmanager.util.EncryptUitl;
 
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 
 public class MydataOperate {
-	private SQLiteDatabase pwdb;
-	private String defkey="123456";
-	
-	public  List<Map<String,Object>> queryOpe(Context context ,String st,String key,String dbname){
-		pwdb=MyDataDB.getSQLiteDatabase(context,dbname);
-		List<Map<String,Object>> arr_list = new ArrayList<Map<String,Object>>();
+
+	//插入
+	public void insertAll(Context context,MyData mdata,String key){
+		SQLiteDatabase pwdb=MyDataDB.getSQLiteDatabase(context);//打开数据库
+		String st=null;
+		String[] values;
+		//判断key的值
+		String defkey=null;
 		if(key!=null||!("".equals(key))){
 			defkey=key;
 		}else{
 			defkey="123456";
 		}
-		//如果为空则查询全部数据
-		if(("".equals(st))||st==null){
-			arr_list=MyDataDB.queryDB(pwdb, st);
+		//如果数据不为空则插入数据
+		if(mdata.title!=null){
+			st="insert into pwtb (title,user,password,note) values (?,?,?,?)";
+			values=new String[]{mdata.title,mdata.user,mdata.password,mdata.note};
+		}//否则插入密码
+		else {
+			//如果登陆密码不为空，则设置登陆密码
+			if(mdata.lgpassword!=null){
+				st="insert into ustb lgpassword value (?)";
+				values=new String[]{EncryptUitl.encode(mdata.lgpassword,defkey)};
+			}//否则设置加密密码
+			else{
+				st="insert into ustb itpassword value (?)";
+				values=new String[]{EncryptUitl.encode(mdata.itpassword,defkey)};
+			}
 		}
-		
-		return arr_list;
+
+		MyDataDB.insertDB(pwdb,st,values);//插入数据
 	}
 
+	//查询数据
+	public  List<Map<String,Object>> queryData(Context context ,String title,String key){
+		SQLiteDatabase pwdb=MyDataDB.getSQLiteDatabase(context);//打开数据库
+		List<Map<String,Object>> arr_list = new ArrayList<Map<String,Object>>();//接收查询结果集合
+		List<Map<String,Object>> out_list = new ArrayList<Map<String,Object>>();//输出查询结果集合
+		//判断key的值
+		String defkey=null;
+		if(key!=null||!("".equals(key))){
+			defkey=key;
+		}else{
+			defkey="123456";
+		}
+		//查询结果
+		//如果不为空则查询特定数据
+		if(title!=null){
+			String st="select * from pwtb where title=?";
+			String[] vaules=new String[]{title};
+			arr_list=MyDataDB.queryDB(pwdb,st,vaules);
+			//如果查询结果为空
+			if(arr_list==null){
+				out_list=null;
+			}//如果查询结果不为空
+			else{
+				int size=arr_list.size();
+				Map<String, Object> map =new HashMap<String, Object>();
+				//对查询结果当中的密码进行解码后再放入到集合当中返回
+				for(int i=0;i<size;i++){
+					map=arr_list.get(i);
+					map.put("password",EncryptUitl.decode(map.get("password").toString(), defkey));
+					out_list.add(map);
+				}
+			}
+		}//如果为空则查询全部数据
+		else{
+			String st="select * from pwtb";
+			String[] vaules=null;
+			arr_list=MyDataDB.queryDB(pwdb,st,vaules);
+			//如果查询结果为空
+			if(arr_list==null){
+				out_list=null;
+			}//如果查询结果不为空
+			else{
+				int size=arr_list.size();
+				Map<String, Object> map =new HashMap<String, Object>();
+				//对查询结果当中的密码进行解码后再放入到集合当中返回
+				for(int i=0;i<size;i++){
+					map=arr_list.get(i);
+					map.put("password",EncryptUitl.decode(map.get("password").toString(), defkey));
+					out_list.add(map);
+				}
+			}
+		}
+		return out_list;
+	}
+	//查询登陆密码
+	public  List<Map<String,Object>> queryLg(Context context ,String lgpassword,String key){
+		SQLiteDatabase pwdb=MyDataDB.getSQLiteDatabase(context);//打开数据库
+		List<Map<String,Object>> arr_list = new ArrayList<Map<String,Object>>();//接收查询结果集合
+		List<Map<String,Object>> out_list = new ArrayList<Map<String,Object>>();//输出查询结果集合
+		//判断key的值
+		String defkey=null;
+		if(key!=null||!("".equals(key))){
+			defkey=key;
+		}else{
+			defkey="123456";
+		}
+		//查询结果
+		String st="select * from ustb where lgpassword=?";
+		String[] vaules=new String[]{EncryptUitl.encode(lgpassword,defkey)};
+		arr_list=MyDataDB.queryDB(pwdb,st,vaules);
+		//如果查询结果为空
+		if(arr_list==null){
+			out_list=null;
+		}//如果查询结果不为空
+		else{
+			int size=arr_list.size();
+			Map<String, Object> map =new HashMap<String, Object>();
+			//对查询结果当中的密码进行解码后再放入到集合当中返回
+			for(int i=0;i<size;i++){
+				map=arr_list.get(i);
+				map.put("lgpassword",EncryptUitl.decode(map.get("lgpassword").toString(), defkey));
+				out_list.add(map);
+			}
+		}
+		return out_list;
+	}
+	//查询加密密码
+	public  List<Map<String,Object>> queryIt(Context context ,String itpassword,String key){
+		SQLiteDatabase pwdb=MyDataDB.getSQLiteDatabase(context);//打开数据库
+		List<Map<String,Object>> arr_list = new ArrayList<Map<String,Object>>();//接收查询结果集合
+		List<Map<String,Object>> out_list = new ArrayList<Map<String,Object>>();//输出查询结果集合
+		//判断key的值
+		String defkey=null;
+		if(key!=null||!("".equals(key))){
+			defkey=key;
+		}else{
+			defkey="123456";
+		}
+		//查询结果
+		String st="select * from ustb where itpassword=?";
+		String[] vaules=new String[]{EncryptUitl.encode(itpassword,defkey)};
+		arr_list=MyDataDB.queryDB(pwdb,st,vaules);
+		//如果查询结果为空
+		if(arr_list==null){
+			out_list=null;
+		}//如果查询结果不为空
+		else{
+			int size=arr_list.size();
+			Map<String, Object> map =new HashMap<String, Object>();
+			//对查询结果当中的密码进行解码后再放入到集合当中返回
+			for(int i=0;i<size;i++){
+				map=arr_list.get(i);
+				map.put("itpassword",EncryptUitl.decode(map.get("itpassword").toString(), defkey));
+				out_list.add(map);
+			}
+		}
+		return out_list;
+	}
+	//更新
+	public void updateOpe(Context context,MyData mdata,String title,String key){
+		SQLiteDatabase pwdb=MyDataDB.getSQLiteDatabase(context);//打开数据库
+		String st=null;
+		String[] values=null;
+		//判断key的值
+		String defkey=null;
+		if(key!=null||!("".equals(key))){
+			defkey=key;
+		}else{
+			defkey="123456";
+		}
+		//如果数据不为空则更新数据
+		if(mdata.title!=null){
+			st="update pwtb set title=?,user=?,password=?,note=? where title=?";
+			values=new String[]{mdata.title,mdata.user,EncryptUitl.encode(mdata.password,defkey),mdata.note,title};
+		}//否则更新密码
+		else {
+			//如果登陆密码不为空，则更新登陆密码
+			if(mdata.lgpassword!=null){
+				st="update ustb set lgpassword=? where lgpassword=?";
+				values=new String[]{EncryptUitl.encode(mdata.lgpassword,defkey)};
+			}//否则更新加密密码
+			else if(mdata.itpassword!=null){
+				st="update ustb set itpassword=? where itpassword=?";
+				values=new String[]{EncryptUitl.encode(mdata.itpassword,defkey)};
+			}
+		}
+		MyDataDB.updateDB(pwdb, st, values);
+	}
+
+	//删除数据
+	public void deleteOpe(Context context ,String title,String key){
+		SQLiteDatabase pwdb=MyDataDB.getSQLiteDatabase(context);//打开数据库
+		String st=null;
+		String[] values=null;
+		//如果title不为空，则删除特定数据
+		if(title!=null){
+			st="delete from pwtb where title=?";
+			values=new String[]{title};
+			MyDataDB.deleteDB(pwdb, st, values);
+		}//如果为空则删除全部数据
+		else{
+			st="delete * from pwtb";
+		}
+		MyDataDB.deleteDB(pwdb, st, values);
+	}
+	//删除登陆密码
+	//删除加密密码
 }
